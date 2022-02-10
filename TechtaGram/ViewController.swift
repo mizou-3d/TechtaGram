5d626e48ff4d301c6243b61cc3bbd042293e3b9d
SayakaMizote <mizou.3d@gmail.com>
2022-02-11 03:48:16 +0900

    Initial Commit

---
+++ TechtaGram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TechtaGram
+//
+//  Created by 溝手彩加 on 2022/02/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
